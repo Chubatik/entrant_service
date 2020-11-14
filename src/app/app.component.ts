89bb6d199da8799b8dd5af879d688a661921e8a2
Chubatik <49166879+Chubatik@users.noreply.github.com>
2020-11-14 15:47:28 +0200

    proxy added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {OnInit} from '@angular/core';
+import {HttpService} from './services/http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+ constructor(private httpService: HttpService) {
+ }
   title = 'entrantService';
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.httpService.get('/api/test').subscribe();
+  }
 }
